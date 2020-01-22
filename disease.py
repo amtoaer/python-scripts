81c853a539d7653b794d3bbdd86512a217f4a7b8
JeasonLau <b1361974998@gmail.com>
2020-01-22 22:32:20 +0800

    fix disease.py again

--- disease.py
+++ disease.py
@@ -15,15 +15,15 @@ def main(area):
     html.encoding = 'utf=8'
     soup = BeautifulSoup(html.text, features='lxml')
     # 正则表达式
-    re1 = r'全国：?\s*(.+)\s'
-    re2 = r'(.+?[省,市,区,门,港])\s+(.+)$'
+    re1 = r'全国：?\s*(.+)$'
+    re2 = r'(.+?)\s+(.+)$'
     # 全国信息位于的标签与省市不同，分开处理
     country = soup.findAll('p', class_='confirmedNumber___3WrF5')[0].get_text()
     dic['全国'] = re.search(re1, country).groups()[0]
     # 遍历省市数据，使用正则表达式匹配，构建字典
     for item in soup.findAll('p', class_='descList___3iOuI'):
         content = item.get_text()
-        # 使用try except过滤掉传染源、传播途径等信息
+        # 使用try except忽略某些无关信息
         try:
             temp = re.search(re2, content).groups()
             dic[temp[0]] = temp[1]
@@ -36,7 +36,7 @@ def main(area):
             print(item[1])
             result = True
             break
-    if result == False:
+    if not result:
         print('该地区目前无病例')
 
 
